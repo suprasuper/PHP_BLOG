e2bc49ff25b4bfdba3460d88cd632d325c8992f6
suprasuper <134078308+suprasuper@users.noreply.github.com>
2025-05-23 11:55:29 +0200

    js optionnel

--- config/assets.php
+++ config/assets.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'css' => [
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', // Bootstrap via CDN
+        //'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', // Bootstrap via CDN
         './assets/css/styles.css' // CSS perso
     ],
     'js' => [
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' // Bootstrap JS
+        //'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' // Bootstrap JS
     ]
 ];
